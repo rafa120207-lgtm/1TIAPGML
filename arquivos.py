3e5398354579c54cb742aba86f136f7247feae10
rafa120207-lgtm <rafa120207@gmail.com>
2026-04-14 09:30:57 -0300

    arquivos

--- arquivos.py
+++ arquivos.py
@@ -7,3 +7,8 @@ with open('arquivo.txt','r+') as file:
 
 with open('arquivo.txt','a') as file:
     file.write('\nMais uma linha no final do arquivo.')
+
+with open('arquivo.txt','w') as file:
+    file.write('Linha 1\n')
+    file.write('Linha 2\n')
+    file.write('Linha 3\n')
