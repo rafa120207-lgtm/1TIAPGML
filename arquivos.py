c498f2f2e0ff51b2b9b86495aeb184ca70143e22
rafa120207-lgtm <rafa120207@gmail.com>
2026-04-16 08:11:16 -0300

    aula 16.04

--- arquivos.py
+++ arquivos.py
@@ -1,14 +1,11 @@
-with open('arquivo.txt','w') as file:
-    file.write('Olá, mundo!')
-
-with open('arquivo.txt','r+') as file:
+with open('arquivo.txt', 'r') as file:
     conteudo = file.read()
-    file.write('\nAdicionando mais conteúdo.')  
+    print(conteudo)
 
-with open('arquivo.txt','a') as file:
-    file.write('\nMais uma linha no final do arquivo.')
-
-with open('arquivo.txt','w') as file:
-    file.write('Linha 1\n')
-    file.write('Linha 2\n')
-    file.write('Linha 3\n')
+with open('arquivo.txt', 'a') as file:
+    linha1 = file.readline()
+    linha2 = file.readline()
+    linha3 = file.readline()
+    print(linha1)
+    print(linha2)
+    print(linha3)
